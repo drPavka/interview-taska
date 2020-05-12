680de368b0c7e7ab43d5ef241d7b0b4ae619bbe4
Pavlo Dubenko <d.pavka@gmail.com>
2020-05-12 10:32:52 +0300

    duplicate count - version 2

--- duplicate/index.ts
+++ duplicate/index.ts
@@ -1,19 +1,17 @@
-type T = { [k: string]: number };
-
 export function duplicateCount(text: string): number {
-    let result = 0;
     const letters = text.toLowerCase().split('');
-    let s = letters.reduce<{ [k: string]: number }>((a, s: string) => {
-        a[s] = (a[s]) ? a[s] + 1 : 1;
-        return a;
-    }, {});
-
-    for (const property in s) {
-        if (s[property] > 1) result++;
-    }
-    return result;
+    const a = [...new Set<string>(letters)];
+    if(a.length === letters.length) return 0;
+    return a.reduce<number>((agg, c)=>{
+        if(letters.indexOf(c) !== letters.lastIndexOf(c)){
+            agg++;
+        }
+        return agg;
+    }, 0);
 }
 
 console.log(duplicateCount('ABBA'));
+console.log(duplicateCount('indivisibility'));
 console.log(duplicateCount('Indivisibilities'));
 console.log(duplicateCount('abcde'));
+console.log(duplicateCount('aA11'));
